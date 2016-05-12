722147f7feafb90a33f6427b2690a3a4cd55c8bb
opsb <oliver@opsb.co.uk>
2016-05-13 00:10:05 +0200

    Replace logger with redux-logger

--- app/instance-initializers/store.js
+++ app/instance-initializers/store.js
@@ -2,41 +2,19 @@ import reduce from 'ember-screamer/reducer/reduce';
 import Redux from 'npm:redux';
 import optimist from 'npm:redux-optimist';
 import { dispatchChannelActions } from 'ember-screamer/lib/redux-channel-dispatcher';
+import createLogger from 'npm:redux-logger';
 
 let { createStore, compose, applyMiddleware } = Redux;
-
-class StoreProxy {
-  constructor(store) {
-    this._store = store;
-  }
-
-  getState() {
-    return this._store.getState();
-  }
-
-  dispatch(action) {
-    let result = this._store.dispatch(action);
-    console.log('dispatch', action);
-
-    window.state = this.getState();
-    return result;
-  }
-
-  subscribe(...args) {
-    return this._store.subscribe(...args);
-  }
-}
 
 export function initialize(application) {
   let channels = application.lookup('service:channels');
 
-  let store = new StoreProxy(
-    createStore(
-      optimist(reduce),
-      compose(
-        dispatchChannelActions(channels),
-        window.devToolsExtension ? window.devToolsExtension() : f => f
-      )
+  let store = createStore(
+    optimist(reduce),
+    compose(
+      dispatchChannelActions(channels),
+      window.devToolsExtension ? window.devToolsExtension() : f => f,
+      applyMiddleware(createLogger()),
     )
   );
 
